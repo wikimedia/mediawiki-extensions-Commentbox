e30ca410868fcad90872b0b0df795a6e662d88fd
Zoranzoki21 <zorandori4444@gmail.com>
2018-09-14 10:55:51 +0000

    Add aliases for Bosnian, Croatian and Serbian
    Bug: T204332
    Change-Id: I26f246de0d000ccc52beed81dfeeb0cd3a888858

--- Commentbox.alias.php
+++ Commentbox.alias.php
@@ -9,7 +9,27 @@
 
 $specialPageAliases = array();
 
+/** Bosnian (bosanski) */
+$specialPageAliases['bs'] = array(
+	'AddComment' => array( 'DodajKomentar' ),
+);
+
 /** English (English) */
 $specialPageAliases['en'] = array(
 	'AddComment' => array( 'AddComment' ),
+);
+
+/** Croatian (hrvatski) */
+$specialPageAliases['hr'] = array(
+	'AddComment' => array( 'DodajKomentar' ),
+);
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = array(
+	'AddComment' => array( 'ДодајКоментар' ),
+);
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = array(
+	'AddComment' => array( 'DodajKomentar' ),
 );
